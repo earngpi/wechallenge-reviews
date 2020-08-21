8b2e61a4f35e102bed7aa90730c8fa44fef6c231
Issrin Earng P <earngpi@gmail.com>
2020-08-21 17:17:50 +0700

    initialised spring boot project

---
+++ src/test/java/com/wongnai/wechallenge/reviews/ReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wongnai.wechallenge.reviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
