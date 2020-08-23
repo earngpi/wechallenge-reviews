156e9c197ecffbf09fbffb41a6266e206b1f00b2
Issrin Earng P <earngpi@gmail.com>
2020-08-24 02:14:35 +0700

    added elasticsearch support

---
+++ src/main/java/com/wongnai/wechallenge/reviews/config/AppConfig.java
@@ -0,0 +1,36 @@
+package com.wongnai.wechallenge.reviews.config;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+
+@Configuration
+public class AppConfig {
+
+    @Value("${elasticsearch.host}")
+    private String host;
+
+    @Value("${elasticsearch.port}")
+    private String port;
+
+    @Bean
+    public RestHighLevelClient client() {
+        ClientConfiguration clientConfiguration
+                = ClientConfiguration.builder()
+                .connectedTo(host + ":" + port)
+                .build();
+
+        return RestClients.create(clientConfiguration).rest();
+    }
+
+    @Bean
+    public ElasticsearchOperations elasticsearchTemplate() {
+        return new ElasticsearchRestTemplate(client());
+    }
+
+}
