0bb99097159e5d26f16c2374c66f5fd8e5e03fdb
Issrin Earng P <earngpi@gmail.com>
2020-08-25 04:22:54 +0700

    updated unit tests following changes from previous commit

--- src/test/java/com/wongnai/wechallenge/reviews/controller/ReviewControllerTest.java
+++ src/test/java/com/wongnai/wechallenge/reviews/controller/ReviewControllerTest.java
@@ -3,59 +3,58 @@ package com.wongnai.wechallenge.reviews.controller;
 import com.wongnai.wechallenge.reviews.model.Review;
 import com.wongnai.wechallenge.reviews.service.DatasetService;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.client.HttpClientErrorException;
 
 import static org.mockito.Mockito.*;
 
 public class ReviewControllerTest {
 
-    static ReviewsController reviewControllerSpy;
-    static DatasetService datasetServiceMock;
+    static private ReviewsController reviewController;
+    static private DatasetService datasetServiceMock;
 
-    static int testId = 130;
-    static String testFoodName = "food name";
-    static Review testReview = new Review();
+    static private Review review = new Review();
 
-    @BeforeAll
-    public static void init() {
-        datasetServiceMock = Mockito.mock(DatasetService.class);
+    static final private int REVIEW_ID_1 = 1;
+    static final private int REVIEW_ID_2 = 2;
+    static final private String FOOD_NAME = "food name";
+
+    @BeforeEach
+    public void init() {
+        datasetServiceMock = mock(DatasetService.class);
         doReturn(null).when(datasetServiceMock).getReview(anyInt());
         doReturn(null).when(datasetServiceMock).searchReviews(anyString());
         doReturn(null).when(datasetServiceMock).updateReview(any(Review.class));
 
-        reviewControllerSpy = Mockito.spy(ReviewsController.class);
-        ReflectionTestUtils.setField(reviewControllerSpy, "datasetService", datasetServiceMock);
+        reviewController = new ReviewsController(datasetServiceMock);
     }
 
     @Test
     public void testGetReview_datasetServiceMethodInvokedOnce() {
-        reviewControllerSpy.getReview(testId);
-        verify(datasetServiceMock, times(1)).getReview(testId);
+        reviewController.getReview(REVIEW_ID_1);
+        verify(datasetServiceMock, times(1)).getReview(REVIEW_ID_1);
     }
 
     @Test
     public void testSearchReviews_datasetServiceMethodInvokedOnce() {
-        reviewControllerSpy.searchReviews(testFoodName);
-        verify(datasetServiceMock, times(1)).searchReviews(testFoodName);
+        reviewController.searchReviews(FOOD_NAME);
+        verify(datasetServiceMock, times(1)).searchReviews(FOOD_NAME);
     }
 
     @Test
     void testUpdateReview_idsInRequestAndBodyMatch_datasetServiceMethodInvokedOnce() {
-        testReview.setReviewId(testId);
-        reviewControllerSpy.updateReview(testId, testReview);
-        verify(datasetServiceMock, times(1)).updateReview(testReview);
+        review.setReviewId(REVIEW_ID_1);
+        reviewController.updateReview(REVIEW_ID_1, review);
+        verify(datasetServiceMock, times(1)).updateReview(review);
     }
 
     @Test
-    void testUpdateReview_idsRequestAndBodyDiff_exceptionThrown_datasetServiceMethodNotInvoked() {
-        testReview.setReviewId(testId + 1);
+    void testUpdateReview_idsInRequestAndBodyDiff_exceptionThrown_datasetServiceMethodNotInvoked() {
+        review.setReviewId(REVIEW_ID_2);
         Assertions.assertThrows(HttpClientErrorException.class, () -> {
-            reviewControllerSpy.updateReview(testId, testReview);
+            reviewController.updateReview(REVIEW_ID_1, review);
         });
-        verify(datasetServiceMock, times(0)).updateReview(testReview);
+        verify(datasetServiceMock, times(0)).updateReview(any(Review.class));
     }
 }
